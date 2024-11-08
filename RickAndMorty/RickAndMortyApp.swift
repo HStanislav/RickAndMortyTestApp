91f12a126bff6a08bef769bd615dd26f0af245f5
Стас Гринорьев <stas.hryhoriev@gmail.com>
2024-11-07 16:32:29 -0800

    init repo

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Стас Гринорьев on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
